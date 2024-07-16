a9df09505bc18b24af01b76a22d07a841e6a8e9e
Hemant Nimble <nimblehemant@gmail.com>
2024-07-16 21:34:15 +0530

    SignUp with creds.(NextAuth)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <p>Landing Page</p>
+      <Link href="/signin">
+        <button>SignIn</button>
+      </Link>
     </main>
   );
 }
---
+++ app/signin/page.tsx
@@ -0,0 +1,12 @@
+import SignIn from '@/components/SignIn'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <SignIn></SignIn>
+    </>
+  )
+}
+
+export default page
