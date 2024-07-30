fc426a895f313b256aaf4a54095fd5142c8d5a8a
Hemant Nimble <nimblehemant@gmail.com>
2024-07-30 22:14:46 +0530

    vercel fix 3

--- app/api/products/getproducts/route.ts
+++ app/api/products/getproducts/route.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
-export async function GET(res: NextResponse) {
+export async function GET() {
     try {
         const products = await prisma?.products.findMany()
         return NextResponse.json({
