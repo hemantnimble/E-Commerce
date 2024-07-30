7f24b604a509847298617caa13155b233d34b6ea
Hemant Nimble <nimblehemant@gmail.com>
2024-07-30 22:17:22 +0530

    vercel fix 4

--- app/api/cart/add/route.ts
+++ app/api/cart/add/route.ts
@@ -1,37 +1,42 @@
 import { auth } from "@/auth";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
+
 const prisma = new PrismaClient();
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  const session = await auth()
-  const userId = session?.user?.id
-  const { productId, quantity } = await req.json();
+export async function POST(req: NextRequest) {
+    const session = await auth();
+    const userId = session?.user?.id;
 
+    // Return an error response if userId is not defined
+    if (!userId) {
+        return NextResponse.json({ message: "User not authenticated" }, { status: 401 });
+    }
 
-  try {
-    const adCart = await prisma.cartItem.upsert({
-      where: {
-        userId_productId: {
-          userId,
-          productId,
-        },
-      },
-      update: {
-        quantity: {
-          increment: quantity,
-        },
-      },
-      create: {
-        userId,
-        productId,
-        quantity,
-      },
-    })
-    return NextResponse.json({ adCart }, { status: 200 });
+    const { productId, quantity } = await req.json();
 
-  } catch (err) {
-    return NextResponse.json({ message: err }, { status: 500 });
-  }
+    try {
+        const adCart = await prisma.cartItem.upsert({
+            where: {
+                userId_productId: {
+                    userId,
+                    productId,
+                },
+            },
+            update: {
+                quantity: {
+                    increment: quantity,
+                },
+            },
+            create: {
+                userId,
+                productId,
+                quantity,
+            },
+        });
+        return NextResponse.json({ adCart }, { status: 200 });
 
+    } catch (err:any) {
+        return NextResponse.json({ message: err.message }, { status: 500 });
+    }
 }
