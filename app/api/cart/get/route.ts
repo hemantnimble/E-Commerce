ad856d4d9b4132b1d4485f23e6021b68e9f1f5a0
Hemant Nimble <nimblehemant@gmail.com>
2024-07-31 12:24:33 +0530

    fix10

--- app/api/cart/get/route.ts
+++ app/api/cart/get/route.ts
@@ -1,26 +1,26 @@
 // pages/api/cart.ts
-// import { auth } from "@/auth";
-// import { PrismaClient } from "@prisma/client";
+import { auth } from "@/auth";
+import { PrismaClient } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
-  // const session = await auth();
-  // const userId =  session?.user?.id
+  const session = await auth();
+  const userId =  session?.user?.id
 
-  // if (!userId) {
-  //   return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  // }
+  if (!userId) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
 
   try {
-    // // Fetch only the cart items belonging to the logged-in user
-    // const cartItems = await prisma.cartItem.findMany({
-    //   where: { userId }, // Filter cart items by the logged-in user's ID
-    //   include: { product: true }, // Include product details if needed
-    // });
+    // Fetch only the cart items belonging to the logged-in user
+    const cartItems = await prisma.cartItem.findMany({
+      where: { userId }, // Filter cart items by the logged-in user's ID
+      include: { product: true }, // Include product details if needed
+    });
 
-    return NextResponse.json( { status: 200 });
+    return NextResponse.json(cartItems, { status: 200 });
   } catch (err:any) {
     return NextResponse.json({ message: err.message }, { status: 500 });
   }
