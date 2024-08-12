0e1987d5c7ced3081ea2fae541d3e75aa601a89a
Hemant Nimble <nimblehemant@gmail.com>
2024-08-12 15:28:27 +0530

    delete product fix

--- app/api/products/delete/route.ts
+++ app/api/products/delete/route.ts
@@ -1,16 +1,30 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
+
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
         console.log('Request body:', body); 
-        const { id } = body; 
-        const deleteUser = await prisma.products.delete({
+        const { id } = body;
+
+        // Delete related CartItems
+        await prisma.cartItem.deleteMany({
+            where: { productId: id },
+        });
+
+        // Delete related OrderItems
+        await prisma.orderItem.deleteMany({
+            where: { productId: id },
+        });
+
+        // Delete the product
+        const deletedProduct = await prisma.products.delete({
             where: { id },
         });
-        return NextResponse.json(deleteUser, { status: 200 });
+
+        return NextResponse.json(deletedProduct, { status: 200 });
     } catch (err: any) {
         console.error('Error:', err.message); 
         return NextResponse.json({ message: err.message }, { status: 500 });
