22489375243dc2ee4d3f2783b532cfd94c331a01
Hemant Nimble <nimblehemant@gmail.com>
2024-09-01 11:18:23 +0530

    vercel fix '

--- app/user/account/profile/page.tsx
+++ app/user/account/profile/page.tsx
@@ -50,7 +50,7 @@ const Profile: React.FC = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
                         </svg>
                     </div>
-                    <p className="mt-2">Can't remember your current password. <a className="text-sm font-semibold text-blue-600 underline decoration-2" href="#">Recover Account</a></p>
+                    <p className="mt-2">Cant remember your current password. <a className="text-sm font-semibold text-blue-600 underline decoration-2" href="#">Recover Account</a></p>
                     <button className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">Save Password</button>
                     <hr className="mt-4 mb-8" />
                     <div className="mb-10">
