b61b01859dfda269ff21b542f059baf6c21404ef
Hemant Nimble <nimblehemant@gmail.com>
2024-09-05 22:09:45 +0530

    vercel fix tyoe

--- app/api/products/update/route.ts
+++ app/api/products/update/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function PUT(req: NextRequest) {
-    const { id, title, price, images }: { id: string, title: string, price: string, images: string[] } = await req.json();
+    const { id, title, price, images }: { id: string, title: string, price: number, images: string[] } = await req.json();
 
     try {
         const updatedProduct = await prisma.products.update({
