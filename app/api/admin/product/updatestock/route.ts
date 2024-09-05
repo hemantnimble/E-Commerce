925643f9c9df320135876b76bc13e8ca368e0d0a
Hemant Nimble <nimblehemant@gmail.com>
2024-09-05 22:20:59 +0530

    update stock api fix

--- app/api/admin/product/updatestock/route.ts
+++ app/api/admin/product/updatestock/route.ts
@@ -4,7 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function PUT(req: NextRequest) {
-    const { id,stock }: { id: string, stock: number } = await req.json();
+    const { id, stock } = await req.json();
+
+    const stockInt = parseInt(stock, 10);
+
 
     try {
         const updatedProduct = await prisma.products.update({
@@ -12,11 +15,11 @@ export async function PUT(req: NextRequest) {
                 id: id,
             },
             data: {
-                stock  
+                stock: stockInt,  
             },
         });
         return NextResponse.json(updatedProduct, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
-};
+}
