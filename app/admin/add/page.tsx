226fd7d8fa9c3bc4b17f816f7aa42c372e60d619
Hemant Nimble <nimblehemant@gmail.com>
2024-10-15 18:52:54 +0530

    ui changes

--- app/admin/add/page.tsx
+++ app/admin/add/page.tsx
@@ -6,6 +6,7 @@ import "@uploadthing/react/styles.css";
 import { UploadButton } from "@uploadthing/react";
 import { OurFileRouter } from "@/app/api/uploadthing/core";
 import Link from "next/link";
+import { Button } from '@headlessui/react';
 
 type Inputs = {
     title: string
@@ -81,93 +82,74 @@ function AddProduct() {
     );
     return (
         <>
-            <h1 className='text-xl'>Add New Product</h1>
-            <form onSubmit={handleSubmit(onSubmit)} className='w-[30rem] py-6 px-10'>
-                <div>
-                    <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">
-                        Enter Title/Name
-                    </label>
-                    <div className="mt-2">
-                        <input {...register("title", { required: true })}
-                            id="title"
-                            name="title"
-                            type="text"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
-                        Enter Price
-                    </label>
-                    <div className="mt-2">
-                        <input {...register("price", { required: true })}
-                            id="price"
-                            name="price"
-                            type="number"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="category" className="block text-sm font-medium leading-6 text-gray-900">
-                        Enter category
-                    </label>
-                    <div className="mt-2">
-                        <input {...register("category", { required: true })}
-                            id="category"
-                            name="category"
-                            type="text"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="stock" className="block text-sm font-medium leading-6 text-gray-900">
-                        Enter stock
-                    </label>
-                    <div className="mt-2">
-                        <input {...register("stock", { required: true })}
-                            id="stock"
-                            name="stock"
-                            type="number"
-                            required
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-                <div>
-                    <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Add Now
-                    </button>
-                </div>
-                <section className="flex min-h-screen flex-col items-center justify-start p-24">
-                    <UploadButton<OurFileRouter, "imageUploader">
-                        endpoint="imageUploader"
-                        onClientUploadComplete={(res: any) => {
-                            if (res) {
-                                setImages(res);
-                                const json = JSON.stringify(res);
+            <section className="bg-white dark:bg-gray-900">
+                <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                    <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                            <div className="sm:col-span-2">
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
+                                <input {...register("title", { required: true })} type="text" name="title" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required />
+                            </div>
+                            <div className="w-full">
+                                <label htmlFor="stock" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock</label>
+                                <input {...register("stock", { required: true })} type="number" name="stock" id="stock" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="stock" required />
+                            </div>
+                            <div className="w-full">
+                                <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                                <input {...register("price", { required: true })} type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required />
+                            </div>
+                            <div className="sm:col-span-2">
+                                <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product category</label>
+                                <input {...register("category", { required: true })} type="text" name="category" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required />
+                            </div>
+                            {/* <div>
+                                <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                                <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                    <option selected>Select category</option>
+                                    <option value="TV">TV/Monitors</option>
+                                    <option value="PC">PC</option>
+                                    <option value="GA">Gaming/Console</option>
+                                    <option value="PH">Phones</option>
+                                </select>
+                            </div> */}
+                            {/* <div>
+                                <label htmlFor="item-weight" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Item Weight (kg)</label>
+                                <input type="number" name="item-weight" id="item-weight" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={"12"} required />
+                            </div>
+                            <div className="sm:col-span-2">
+                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                                <textarea id="description" rows={8} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here" defaultValue={""} />
+                            </div> */}
+                        </div>
+                        <section className="flex flex-col items-center justify-start pt-4">
+                            <UploadButton<OurFileRouter, "imageUploader">
+                                endpoint="imageUploader"
+                                onClientUploadComplete={(res: any) => {
+                                    if (res) {
+                                        setImages(res);
+                                        const json = JSON.stringify(res);
 
-                            }
-                            alert("Upload Completed");
-                        }}
-                        onUploadError={(error: Error) => {
-                            // Do something with the error
-                            alert(`ERROR! ${error.message}`);
-                        }}
-                    />
-                    {imgList}
-                </section>
-            </form>
+                                    }
+                                    alert("Upload Completed");
+                                }}
+                                onUploadError={(error: Error) => {
+                                    // Do something with the error
+                                    alert(`ERROR! ${error.message}`);
+                                }}
+                            />
+                            {imgList}
+                        </section>
+                        <button type="submit" className="mt-4 flex w-full justify-center rounded-md bg-cyan-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> Add Product </button>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
 
 export default AddProduct
+
+
+
+
