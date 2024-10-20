eb6583c93ec5b90820977863a9fc819571bae2f6
Hemant Nimble <nimblehemant@gmail.com>
2024-10-20 18:22:26 +0530

    vercel 3d check

--- components/Model3d.tsx
+++ components/Model3d.tsx
@@ -2,24 +2,28 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { Suspense } from 'react';
+
 interface ModelProps {
     path: string;
 }
+
 const Model = ({ path }: ModelProps) => {
     const { scene } = useGLTF(path);
     return <primitive object={scene} scale={3} />;
 };
+
 function Model3d({ productName }: { productName: any }) {
     return (
         <div className="w-full h-full">
             <Canvas>
                 <Suspense fallback={null}>
-                    <ambientLight intensity={0.5} />
-                    <directionalLight position={[0, 5, 5]} intensity={1} />
-                    <Model path={`/assets/${productName}.glb`} />
+                    <ambientLight intensity={2} />
+                    <directionalLight position={[0, 5, 5]} intensity={2} />
+                    <Model path={`/assets/lamp.glb`} />
                     <OrbitControls enableZoom={true} />
                 </Suspense>
             </Canvas>
         </div>)
 }
+
 export default Model3d
