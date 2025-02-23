d0197a85b3ec6cdb2a9301f83fa90694dc72c396
Hemant Nimble <nimblehemant@gmail.com>
2025-02-23 21:31:35 +0530

    vercel fix 4

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,44 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { auth } from "@/auth";
+import { withAuth } from "next-auth/middleware";
 
-const protectedRoutes = ["/user/account","/user/checkoutone","/user/checkout"];
+const protectedRoutes = ["/user/account", "/user/checkoutone", "/user/checkout"];
 const adminRoutes = ["/admin"];
 
-export default async function middleware(request: NextRequest) {
-  const session = await auth();
-  const userRole = session?.user?.roles || [];
+export default withAuth(
+  function middleware(request) {
+    const userRole = request.nextauth.token?.roles || [];
 
-  const isProtected = protectedRoutes.some((route) =>
-    request.nextUrl.pathname.startsWith(route)
-  );
+    const isProtected = protectedRoutes.some((route) =>
+      request.nextUrl.pathname.startsWith(route)
+    );
 
-  const isAdminRoute = adminRoutes.some((route) =>
-    request.nextUrl.pathname.startsWith(route)
-  );
+    const isAdminRoute = adminRoutes.some((route) =>
+      request.nextUrl.pathname.startsWith(route)
+    );
 
-  if (!session) {
-    if (isProtected || isAdminRoute) {
+    if (isProtected && !request.nextauth.token) {
+      // Redirect unauthenticated users to the sign-in page
       const absoluteURL = new URL("/user/signin", request.nextUrl.origin);
       return NextResponse.redirect(absoluteURL.toString());
     }
-  } else if (isAdminRoute && !userRole.includes("ADMIN")) {
-    const absoluteURL = new URL("/", request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
-  }
 
-  return NextResponse.next();
-}
+    if (isAdminRoute && !userRole.includes("ADMIN")) {
+      // Redirect non-admin users to the home page
+      const absoluteURL = new URL("/", request.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
+    }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        // Allow access if the user is authenticated
+        return !!token;
+      },
+    },
+  }
+);
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
